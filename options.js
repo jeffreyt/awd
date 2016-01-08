9e99f3c1cfe70863b5a1ebbef4b8b6962a8682e9
Michael Tamashiro <mike.tamashiro@gmail.com>
2016-01-08 15:52:12 -0700

    Added ability to save interval time via text or slider to local storage

--- options.js
+++ options.js
@@ -1,12 +1,48 @@
+var DEFAULT_CHECKTIME = 180.0;
+
 
 window.onload = function(){
-    document.getElementById("import_perform").addEventListener('click',function(){
-        var importText = document.getElementById("text_area").value
-        importPages(importText);
-    });
-    document.getElementById("export_perform").addEventListener('click',function(){
-        document.getElementById("text_area").value = "";
-        exportPages();
-    });
 
+  //loading saved options
+  chrome.storage.local.get("interval_time",function(result){
+    //if saved options dont exist use defaults and save
+    if (typeof result.interval_time == 'undefined'){
+      document.getElementById("check_time").value = DEFAULT_CHECKTIME;
+      document.getElementById("check_time_slider").value = DEFAULT_CHECKTIME;
+      chrome.storage.local.set({"interval_time":DEFAULT_CHECKTIME},function(){
+      });
+    }
+    else{
+      document.getElementById("check_time").value = result.interval_time;
+      document.getElementById("check_time_slider").value = result.interval_time;
+    }
+  });
+
+
+  document.getElementById("import_perform").addEventListener('click',function(){
+      var importText = document.getElementById("text_area").value
+      importPages(importText);
+  });
+  //export button
+  document.getElementById("export_perform").addEventListener('click',function(){
+      document.getElementById("text_area").value = "";
+      exportPages();
+  });
+  //save slider
+      document.getElementById("check_time_slider").addEventListener("change",function(){
+      document.getElementById("check_time").value = document.getElementById("check_time_slider").value;
+  })
+  //save button
+  document.getElementById("save_options").addEventListener("click",function(){
+    //get options to save
+    var intervalTime = parseFloat(document.getElementById("check_time").value);
+    //possible more variables here
+    //save options
+
+    chrome.storage.local.set({"interval_time":intervalTime},function(){
+      console.log("Successfully saved");
+      document.getElementById("check_time_slider").value = intervalTime;
+     })
+
+  })
 }
