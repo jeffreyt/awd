aa1592385cc92ba579fe69adf087da8c514838e5
Michael Tamashiro <mike.tamashiro@gmail.com>
2016-01-11 20:30:06 -0700

    More work on popup menu

--- popup.js
+++ popup.js
@@ -10,7 +10,8 @@ function loadListings(willNotify){
   var main = $('#main_body');
   console.log(willNotify);
   for(i=0;i<willNotify.length;i++){
-    console.log(i);
-    main.append('<div><a href="' +i[1]+ '">' +i[0]+ '</a></div>');
+    console.log(willNotify[i]);
+    main.append('<div><a href="' +willNotify[i][1]+ '" target="_blank">' +willNotify[i][0]+ '</a></div>');
+    main.append('<br><hr>');
   }
 }
